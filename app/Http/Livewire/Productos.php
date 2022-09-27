d4dcb1263b39fac2008b96a19ac32d72149aac74
JavierGiraldo <pac8314@gmail.com>
2022-09-27 16:47:50 -0500

    Definiendo variables en class Productos

--- app/Http/Livewire/Productos.php
+++ app/Http/Livewire/Productos.php
@@ -3,11 +3,17 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Producto;
 
 class Productos extends Component
 {
+    //definimos unas variables
+    public $productos, $descripcion, $cantidad, $id_producto;
+    public $modal = false;
+
     public function render()
     {
+        
         return view('livewire.productos');
     }
 }
