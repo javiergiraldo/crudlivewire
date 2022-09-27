34295ccc29eaaef8c46d5eb6093b23e4484a64f5
JavierGiraldo <pac8314@gmail.com>
2022-09-27 17:49:18 -0500

    Vista Productos (agregado manualmente a BD)

--- resources/views/livewire/productos.blade.php
+++ resources/views/livewire/productos.blade.php
@@ -6,4 +6,17 @@
             <th>Cantidad</th>
         </tr>
     </thead>
+    <tbody>
+        @foreach ($productos as $producto)
+        <tr>
+            <td class="border px-4 py-2">{{ $producto->id }}</td>
+            <td class="border px-4 py-2">{{ $producto->descripcion }}</td>
+            <td class="border px-4 py-2">{{ $producto->cantidad }}</td>
+            <td>
+                <button wire:click="editar({{ $producto->id }})"></button>
+                <button wire:click="borrar({{ $producto->id }})"></button>
+            </td>
+        </tr>            
+        @endforeach
+    </tbody>
 </table>
